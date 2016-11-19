0c71907a294d2ab238117dc4f44d197b2bee73b4
けいそ <sasrai@gmail.com>
2016-11-20 06:22:22 +0900

    レスポンス調整

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const upload = multer({ dest: 'schematics' });
 
 // config
 const schDirectory = './schematics';
+const isDebug = true;
+const accessOrigin = 'http://localhost:8001';
 
 // rootはアクセスできましぇん
 app.get('/', (req, res) => {
@@ -27,10 +29,11 @@ app.get('/+schematics', (req, res) => {
   fs.readdir('./schematics/', (err, files) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    if (isDebug) res.setHeader('Access-Control-Allow-Origin', accessOrigin);
     if (err) { res.end('{status: error}'); }
     else {
       let _files = files.filter(fn => fn.endsWith('.schematic'))
-                        .map(fn => fn.replace(/\.schematic$/, ''));
+                        .map(fn => { return {filename: fn.replace(/\.schematic$/, '') } });
       res.end(JSON.stringify(_files));
     }
   });
@@ -49,10 +52,12 @@ app.get('/+schematics/:sch_name', (req, res) => {
     })).then((data) => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json; charset=utf-8');
+      if (isDebug) res.setHeader('Access-Control-Allow-Origin', accessOrigin);
       res.end(data);
     }).catch((err) => {
       res.statusCode = 403;
       res.setHeader('Content-Type', 'application/json; charset=utf-8');
+      if (isDebug) res.setHeader('Access-Control-Allow-Origin', accessOrigin);
       res.end(JSON.stringify({ status: 'read error', error: err }));
     });
   } else {
@@ -78,6 +83,7 @@ app.post('/+schematics/upload', upload.single('sch_file'), function (req, res, n
     const upload_name = `schematics/${req.file.originalname}`;
 
     // TODO: md5で上書きチェックを追加
+    // TODO: 削除キーで上書きロックを追加
     Promise.resolve(new Promise((resolve, reject) => {
       fs.rename(req.file.path, upload_name, (err) => {
         if (err) reject(err);
@@ -88,6 +94,7 @@ app.post('/+schematics/upload', upload.single('sch_file'), function (req, res, n
       fs.writeFile(`${upload_name}.json`, JSON.stringify( {
         title: req.body.title,
         description: req.body.description,
+        delete_key: req.body.delete_key,
         upload_date: Date.now() } ),
         (err) => {
           if (err) reject(err);
@@ -98,18 +105,26 @@ app.post('/+schematics/upload', upload.single('sch_file'), function (req, res, n
     })).then((result) => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json; charset=utf-8');
+      if (isDebug) res.setHeader('Access-Control-Allow-Origin', accessOrigin);
       res.end(JSON.stringify(result));
     }).catch((err) => {
       res.statusCode = 500;
       res.setHeader('Content-Type', 'application/json; charset=utf-8');
+      if (isDebug) res.setHeader('Access-Control-Allow-Origin', accessOrigin);
       res.end(JSON.stringify({ status: 'error', message: err.message, error: err }));
     });
 
   } else {
     res.statusCode = 400;
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    if (isDebug) res.setHeader('Access-Control-Allow-Origin', accessOrigin);
     res.end('{status:error,message:"Invalid API Syntax"}');
   }
+});
+
+// TODO: deleteメソッドの実装
+// 削除キーが一致、もしくは未設定の場合に削除実行
+app.get('/+schematics/:sch_name/download', (req, res) => {
 });
 
 app.listen(8334, () => {
